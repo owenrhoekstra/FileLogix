d84f5ced388e411ec00fc0e67e16162d20b6582c
owenrhoekstra <owenhoekstra@icloud.com>
2026-04-10 19:26:52 -0400

    added env var for website access

--- backend/authentication/config.go
+++ backend/authentication/config.go
@@ -2,6 +2,7 @@ package authentication
 
 import (
 	"github.com/go-webauthn/webauthn/webauthn"
+	"os"
 )
 
 var webAuthn *webauthn.WebAuthn
@@ -9,10 +10,22 @@ var webAuthn *webauthn.WebAuthn
 func InitWebAuthn() {
 	var err error
 
+	// Get RPID from environment variable, fallback to default
+	rpid := os.Getenv("WEBAUTHN_RPID")
+	if rpid == "" {
+		rpid = "orh-home-server.tailac3f56.ts.net" // default for dev
+	}
+
+	// Get RP Origins from environment variable, fallback to default
+	rpOrigin := os.Getenv("WEBAUTHN_RP_ORIGIN")
+	if rpOrigin == "" {
+		rpOrigin = "https://orh-home-server.tailac3f56.ts.net" // default for dev
+	}
+
 	webAuthn, err = webauthn.New(&webauthn.Config{
 		RPDisplayName: "FileLogix",
-		RPID:          "orh-home-server.tailac3f56.ts.net",
-		RPOrigins:     []string{"https://orh-home-server.tailac3f56.ts.net"},
+		RPID:          rpid,
+		RPOrigins:     []string{rpOrigin},
 	})
 	if err != nil {
 		panic(err)
